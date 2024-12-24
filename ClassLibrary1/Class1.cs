cd842a0e219e997ae9bd4f9113f1ddc68bdd8530
Z1vran <aliman0198a@gmail.com>
2024-12-24 09:51:50 +0300

    Никонов

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    namespace GarageLibrary
+    {
+        public class Garage
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public List<Car> Cars { get; set; } = new List<Car>();
+
+            public void ShowAll()
+            {
+                Cars.Sort((x, y) => x.Number.CompareTo(y.Number)); // Сортировка по номеру
+                Console.WriteLine($"Название гаража: {Name}");
+                Console.WriteLine($"Адрес гаража: {Address}");
+                Console.WriteLine("Машины в гараже:");
+                foreach (Car car in Cars)
+                {
+                    car.Show();
+                }
+            }
+        }
+
+        public class Car
+        {
+            public string Owner { get; set; }
+            public string Number { get; set; }
+
+            public void Show()
+            {
+                Console.WriteLine($"  Владелец: {Owner}, Номер: {Number}");// выполнил Никонов Алексей Владиславович
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary1.GarageLibrary;
+using ClassLibrary1.GarageLibrary;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Garage myGarage = new Garage { Name = "Мой гараж", Address = "ул. Юбилейная, 123" };
+
+        myGarage.Cars.Add(new Car { Owner = "Алексей Никонов", Number = "В644ТК" });
+        myGarage.Cars.Add(new Car { Owner = "Артем Бузовкин", Number = "В777ОР" });
+        myGarage.Cars.Add(new Car { Owner = "Иван Хилков", Number = "А111АА" });
+
+        myGarage.ShowAll();
+        Console.ReadKey(); // Делал Никонов Алексей
+    }
+}
